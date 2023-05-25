408a03a67622bebb4eb542dabe770f2eee91bce6
Bobr_ba88@student.itstep.org <Bobr_ba88@student.itstep.org>
2023-05-25 11:09:13 +0300

    Добавьте файлы проекта.

---
+++ commandPattern/commandPattern.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+
+// Command Pattern (Бот телеграм)
+
+class Command {
+
+public:
+    virtual void app() = 0;
+
+	~Command() {
+	}
+
+};
+
+class RegisterCommand : public Command {
+
+	std::string reciever;
+
+public:
+
+	RegisterCommand(const std::string& reciever) : reciever(reciever) {}
+
+	void app() override {
+		std::cout << "Запуск регистрации пользователя. Получатель: " << reciever << std::endl;
+	}
+
+};
+
+class CheckUserCommand : public Command {
+
+	std::string reciever;
+	
+public:
+
+	CheckUserCommand(const std::string &reciever) : reciever(reciever) {}
+
+	void app() override {
+		std::cout << "Запуск проверки пользователя. Получатель: " << reciever << std::endl;
+	}
+
+};
+
+class DBAddCommand : public Command {
+
+	std::vector<std::map<std::string, std::string>> usersList{};
+
+public:
+
+	DBAddCommand(const std::vector<std::map<std::string, std::string>>& usersList) : usersList(usersList) {}
+
+	void app() override {
+
+		for (const auto& user : usersList) {
+			std::cout << "Никнейм: " << user.at("nick") << " || Доступ: " << user.at("access") << std::endl;
+		}
+
+	}
+
+};
+
+
+class Sender {
+
+	std::vector<Command*> commands;
+
+public:
+
+	void addCommand(Command* command) {
+		commands.push_back(command);
+	}
+
+	void StartCommands() {
+
+		for (Command* command : commands) {
+
+			command->app();
+
+		}
+	}
+
+};
+
+
+int main() {
+	
+	setlocale(LC_ALL, "ru");
+
+	//virtual tg bot
+
+	std::vector < std::map<std::string, std::string>> UserDB{
+
+		{{"nick", "Kirill"}, {"access", "Administrator"}},
+		{{"nick", "Anna"}, {"access", "Default"}},
+		{{"nick", "Kira"}, {"access", "Default"}}
+
+	};
+
+
+
+	Command* app1 = new DBAddCommand(UserDB);
+	Command* app2 = new RegisterCommand("User1");
+	Command* app3= new CheckUserCommand("User2");
+
+	Sender sender;
+
+	sender.addCommand(app1);
+	sender.addCommand(app2);
+	sender.addCommand(app3);
+
+	sender.StartCommands();
+
+	delete app1;
+	delete app2;
+	delete app3;
+    
+	return 0;
+}
